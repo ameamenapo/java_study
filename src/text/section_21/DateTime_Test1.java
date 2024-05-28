8cd34d3b8f4e4411a5dbeb69d1cad928baa6919b
Asuka Hirano <asukajo12345@gmail.com>
2024-05-28 21:03:07 +0900

    2024/5/28

---
+++ src/text/section_21/DateTime_Test1.java
@@ -0,0 +1,15 @@
+package text.section_21;
+
+import java.util.Date;
+
+public class DateTime_Test1 {
+
+	public static void main(String[] args) {
+		//現在日時を取得する
+		Date date = new Date();
+		System.out.println(date);
+		
+
+	}
+
+}
