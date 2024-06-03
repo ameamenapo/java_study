ba5db21e1140f2e0543f27a3efaba7787bbb949d
Asuka Hirano <asukajo12345@gmail.com>
2024-06-03 22:44:25 +0900

    2024/6/3

---
+++ src/text/section_25/LambdaExec_Test1.java
@@ -0,0 +1,12 @@
+package text.section_25;
+
+public class LambdaExec_Test1 {
+
+	public static void main(String[] args) {
+		//関数型インターフェースのインスタンス
+				LambdaImp_Test1 lambda = new LambdaImp_Test1();
+				lambda.test1("関数型インターフェースを使って実装しました");
+
+	}
+
+}
